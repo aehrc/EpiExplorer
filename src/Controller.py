672a405fed69e6fd60ff5f70b198705695f45ff2
Milindi-Kodikara <s3667779@student.rmit.edu.au>
2020-02-25 15:28:59 +1100

    Filtering works

--- src/Controller.py
+++ src/Controller.py
@@ -21,51 +21,65 @@ class Controller:
     # Takes the arguments: GUI filtering requirements, whether graph should be updated or if it's the first time,
     # If the network type is Interaction or Edge
     def perform_core_functionality(self, core_details, update, interaction_or_edge):
-        # Validate input file
-        input_files = core_details.iat[0, 0]
-        input_files = input_files.split()
-        for input_file in input_files:
-            print(input_file)
-            read_write_data = ReadWriteData(input_file, core_details.iat[0, 1], core_details.iat[0, 2])
-            valid_input_file = read_write_data.validate_input_file()
-            valid_annotation_file = read_write_data.validate_annotation_file()
-            files_loaded = False
-            if valid_input_file and valid_annotation_file:
-                print('The input file, {}, has been successfully validated.'
-                      .format(input_file))
+        global integration
+        files_loaded = False
+        if not update:
+            # Validate input file
+            input_files = core_details.iat[0, 0]
+            input_files = input_files.split()
+            for input_file in input_files:
+                print(input_file)
+                read_write_data = ReadWriteData(input_file, core_details.iat[0, 1], core_details.iat[0, 2])
+                valid_input_file = read_write_data.validate_input_file()
+                valid_annotation_file = read_write_data.validate_annotation_file()
+                files_loaded = False
+                if valid_input_file and valid_annotation_file:
+                    print('The input file, {}, has been successfully validated.'
+                          .format(input_file))
 
-                print('The annotation file, {}, has been successfully loaded.'
-                      .format(core_details.iat[0, 1]))
+                    print('The annotation file, {}, has been successfully loaded.'
+                          .format(core_details.iat[0, 1]))
 
-                files_loaded = True
+                    files_loaded = True
 
-                # Get the node_df, edge_df and if the functions were successful
-                read_write_done = read_write_data.get_dataframes(interaction_or_edge)
-                if read_write_done[2]:
-                    print(
-                        'The input file, {}, has been successfully loaded '
-                        'and the output file has been created successfully.'.format(
-                            input_file))
-                    print('Send data to Cytoscape.')
+                    # Get the node_df, edge_df and if the functions were successful
+                    read_write_done = read_write_data.get_dataframes(interaction_or_edge)
+                    if read_write_done[2]:
+                        print(
+                            'The input file, {}, has been successfully loaded '
+                            'and the output file has been created successfully.'.format(
+                                input_file))
+                        print('Send data to Cytoscape.')
 
-                    # Send the DataFrames in order to create the json file and create the network
-                    integration = CytoscapeIntegration(read_write_done[0], read_write_done[1], core_details,
-                                                       interaction_or_edge)
-                    # Call function and determine if cytoscape worked
-                    cytoscape_successful = integration.cytoscape_successful(update)
+                        # Send the DataFrames in order to create the json file and create the network
+                        if integration is None:
+                            integration = CytoscapeIntegration(read_write_done[0], read_write_done[1],
+                                                               interaction_or_edge)
+                        # Call function and determine if cytoscape worked
+                        cytoscape_successful = integration.cytoscape_successful(update, core_details)
 
-                    if cytoscape_successful:
-                        print('Successful creation of network!')
+                        if cytoscape_successful:
+                            print('Successful creation of network!')
+                        else:
+                            print('Network creation unsuccessful, please make sure that Cytoscape is running in the background')
                     else:
-                        print('Network creation unsuccessful, please make sure that Cytoscape is running in the background')
-                else:
-                    print('Error has occurred in Read and/or Write of the file.')
-            elif not valid_input_file:
-                print('Error found in input file format.')
-                files_loaded = False
-            elif not valid_annotation_file:
-                print('Error found when loading the annotation file')
-                files_loaded = False
-            print('___________________________________')
+                        print('Error has occurred in Read and/or Write of the file.')
+                elif not valid_input_file:
+                    print('Error found in input file format.')
+                    files_loaded = False
+                elif not valid_annotation_file:
+                    print('Error found when loading the annotation file')
+                    files_loaded = False
+                print('___________________________________')
+        elif update:
+            print('Calliiinnggg update')
+            cytoscape_successful = integration.cytoscape_successful(update, core_details)
+            if cytoscape_successful:
+                print('Successful creation of network!')
+            else:
+                print('Network creation unsuccessful, please make sure that Cytoscape is running in the background')
 
         return files_loaded
+
+
+integration = None
--- src/CytoscapeIntegration.py
+++ src/CytoscapeIntegration.py
@@ -8,13 +8,16 @@ import pandas as pd
 
 
 class CytoscapeIntegration:
-    def __init__(self, node_df, edge_df, core_details, interaction_or_edge):
+
+    def __init__(self, node_df, edge_df, interaction_or_edge):
         self.node_df = node_df
         self.edge_df = edge_df
-        self.core_details = core_details
         self.interaction_or_edge = interaction_or_edge
         self.json_file_name = 'json_file.json'
         self.json_file_path = os.path.join('../SampleData/InteractionGraph/', self.json_file_name)
+        self.cy = CyRestClient()
+        #self.node_edge_network = self.cy.network
+        self.node_edge_network = None # = self.cy.network.create_from(self.json_file_path)
 
     # Method to convert the DataFrames to a json object and save as a .json file
     def dataframe_to_json(self):
@@ -58,101 +61,100 @@ class CytoscapeIntegration:
         return True
 
     # Method to create the network and add styles
-    def cytoscape_successful(self, update):
+    def cytoscape_successful(self, update, core_details):
 
         cytoscape_successful = True
-
-        # Create client
-        cy = CyRestClient()
-        # Clear current session
-        cy.session.delete()
-
-        # Convert DataFrame to json file and save file
-        self.dataframe_to_json()
-
-        # Create network from json file
-        node_edge_network = cy.network.create_from(self.json_file_path)
-
-        cy.layout.apply(network=node_edge_network)
-
-        # Add styles to the network
-        my_style = cy.style.create('Epi_Explorer_style')
-
-        # Discrete mappings for specific regions
-        order_colour_key_value_pair = {
-            '1': '#c99e10',
-            '2': '#9b4f0f',
-            '3': '#1e434c',
-            '4': '#8d230f'
-        }
-
-        overlap_colour_key_value_pair = {
-            'Protein_coding': '#c99e10',
-            'Non_coding': '#9b4f0f',
-            'Intergenic': '#1e434c'
-        }
-
-        edge_order_colour_key_value_pair = {
-            '2': '#9b4f0f',
-            '3': '#1e434c',
-            '4': '#8d230f'
-        }
-
-        edge_order_size_key_value_pair = {
-            '2': '5.0',
-            '3': '3.0',
-            '4': '1.0'
-        }
-
-        order_size_key_value_pair = {
-            '1': '25.0',
-            '2': '35.0',
-            '3': '40.0',
-            '4': '50.0'
-        }
-
-        order_shape_key_value_pair = {
-            '1': 'Ellipse',
-            '2': 'Diamond',
-            '3': 'Triangle',
-            '4': 'Hexagon'
-        }
-
-        type_colour_variation = [
-            {
-                'value': '1.0',
-                'lesser': '#9b4f0f',
-                'equal': '#9b4f0f',
-                'greater': '#9b4f0f'
-            },
-            {
-                'value': '20.0',
-                'lesser': '#8d230f',
-                'equal': '#8d230f',
-                'greater': '#8d230f'
-            }
-        ]
-
-        new_styles = {
-            'NODE_FILL_COLOR': '#363636',
-            'NODE_SIZE': 10,
-            'NODE_BORDER_WIDTH': 0,
-            'NODE_TRANSPARENCY': 255,
-            'NODE_LABEL_COLOR': '#323334',
-
-            'EDGE_WIDTH': 3,
-            'EDGE_STROKE_UNSELECTED_PAINT': '#a9a9a9',
-            'EDGE_LINE_TYPE': 'SOLID',
-            'EDGE_TRANSPARENCY': 120,
-
-            'NETWORK_BACKGROUND_PAINT': 'white'
-        }
-
-        my_style.update_defaults(new_styles)
-
-        # If the GUI is being loaded for the first time
-        # Then create network with 'default' styles
         if not update:
+            # Create client
+
+            # Clear current session
+            #cy.session.delete()
+
+            # Convert DataFrame to json file and save file
+            self.dataframe_to_json()
+
+            # Create network from json file
+            print(">>>> Create Network")
+            self.node_edge_network = self.cy.network.create_from(self.json_file_path)
+
+            self.cy.layout.apply(network=self.node_edge_network)
+
+            # Add styles to the network
+            my_style = self.cy.style.create('Epi_Explorer_style')
+
+            # Discrete mappings for specific regions
+            order_colour_key_value_pair = {
+                '1': '#c99e10',
+                '2': '#9b4f0f',
+                '3': '#1e434c',
+                '4': '#8d230f'
+            }
+
+            overlap_colour_key_value_pair = {
+                'Protein_coding': '#c99e10',
+                'Non_coding': '#9b4f0f',
+                'Intergenic': '#1e434c'
+            }
+
+            edge_order_colour_key_value_pair = {
+                '2': '#9b4f0f',
+                '3': '#1e434c',
+                '4': '#8d230f'
+            }
+
+            edge_order_size_key_value_pair = {
+                '2': '5.0',
+                '3': '3.0',
+                '4': '1.0'
+            }
+
+            order_size_key_value_pair = {
+                '1': '25.0',
+                '2': '35.0',
+                '3': '40.0',
+                '4': '50.0'
+            }
+
+            order_shape_key_value_pair = {
+                '1': 'Ellipse',
+                '2': 'Diamond',
+                '3': 'Triangle',
+                '4': 'Hexagon'
+            }
+
+            type_colour_variation = [
+                {
+                    'value': '1.0',
+                    'lesser': '#9b4f0f',
+                    'equal': '#9b4f0f',
+                    'greater': '#9b4f0f'
+                },
+                {
+                    'value': '20.0',
+                    'lesser': '#8d230f',
+                    'equal': '#8d230f',
+                    'greater': '#8d230f'
+                }
+            ]
+            new_styles = {
+                'NODE_FILL_COLOR': '#363636',
+                'NODE_SIZE': 10,
+                'NODE_BORDER_WIDTH': 0,
+                'NODE_TRANSPARENCY': 255,
+                'NODE_LABEL_COLOR': '#323334',
+
+                'EDGE_WIDTH': 3,
+                'EDGE_STROKE_UNSELECTED_PAINT': '#a9a9a9',
+                'EDGE_LINE_TYPE': 'SOLID',
+                'EDGE_TRANSPARENCY': 120,
+
+                'NETWORK_BACKGROUND_PAINT': 'white'
+            }
+            # If the GUI is being loaded for the first time
+            # Then create network with 'default' styles
+
+            my_style.update_defaults(new_styles)
+
             # Add these styles only if the network type is Interaction
             if self.interaction_or_edge == 1:
                 my_style.create_discrete_mapping(column='order', col_type='String', vp='NODE_FILL_COLOR',
@@ -170,70 +172,118 @@ class CytoscapeIntegration:
 
             my_style.create_discrete_mapping(column='order', col_type='String', vp='EDGE_WIDTH',
                                              mappings=edge_order_size_key_value_pair)
+
+            self.cy.style.apply(my_style, self.node_edge_network)
+
+
         # TODO when reset/ submit same thing as what the user specified gets loaded
         elif update:
             print('Update styles')
-            print(self.core_details)
+            print(core_details)
             # TODO add code for the rest of the styles
             # TODO check if the column exists in the annotation file
-            if 'node_colour' in self.core_details.columns:
+            if 'node_colour' in core_details.columns:
                 print('Styling node colour')
-                update_type = self.core_details.at[0, 'node_colour']
+                update_type = core_details.at[0, 'node_colour']
 
-                if update_type == 'Order' or update_type == 'Default':
-                    my_style.create_discrete_mapping(column='order', col_type='String', vp='NODE_FILL_COLOR',
-                                                     mappings=order_colour_key_value_pair)
-                elif update_type == 'Type':
-                    # https://github.com/cytoscape/cytoscape-automation/blob/master/for-scripters/Python/basic-fundamentals.ipynb
-                    my_style.create_continuous_mapping(column='Alpha', col_type='Double', vp='NODE_FILL_COLOR',
-                                                       points=type_colour_variation)
+                # if update_type == 'Order' or update_type == 'Default':
+                #     my_style.create_discrete_mapping(column='order', col_type='String', vp='NODE_FILL_COLOR',
+                #                                      mappings=order_colour_key_value_pair)
+                # elif update_type == 'Type':
+                #     # https://github.com/cytoscape/cytoscape-automation/blob/master/for-scripters/Python/basic-fundamentals.ipynb
+                #     my_style.create_continuous_mapping(column='Alpha', col_type='Double', vp='NODE_FILL_COLOR',
+                #                                        points=type_colour_variation)
+                #
+                # elif update_type == 'Overlap':
+                #     my_style.create_discrete_mapping(column='Overlap', col_type='String', vp='NODE_FILL_COLOR',
+                #                                      mappings=overlap_colour_key_value_pair)
 
-                elif update_type == 'Overlap':
-                    my_style.create_discrete_mapping(column='Overlap', col_type='String', vp='NODE_FILL_COLOR',
-                                                     mappings=overlap_colour_key_value_pair)
-
-            if 'node_size' in self.core_details.columns:
+            if 'node_size' in core_details.columns:
                 print('Styling node size')
-                update_type = self.core_details.at[0, 'node_size']
+                update_type = core_details.at[0, 'node_size']
 
                 if update_type == 'Order' or update_type == 'Default':
-                    my_style.create_discrete_mapping(column='order', col_type='String', vp='NODE_SIZE',
-                                                     mappings=order_size_key_value_pair)
+                    print('')
+                    # my_style.create_discrete_mapping(column='order', col_type='String', vp='NODE_SIZE',
+                    #                                  mappings=order_size_key_value_pair)
 
                 elif update_type == 'Type':
                     # TODO add continuous mapping for Alpha/ Beta values
                     print('TODO')
 
             # Code for querying out data
-            if 'query' in self.core_details.columns:
-                if 'hide' in self.core_details.columns:
+            if 'query' in core_details.columns:
+                if 'hide' in core_details.columns:
                     # TODO check if hide is set to True first omh
                     self.filter_data()
 
-                if 'show' in self.core_details.columns:
+                if 'show' in core_details.columns:
                     self.filter_data()
 
-                if 'highlight' in self.core_details.columns:
+                if 'highlight' in core_details.columns:
                     self.filter_data()
 
-                if 'gray' in self.core_details.columns:
+                if 'gray' in core_details.columns:
                     # Link: https://github.com/cytoscape/cytoscape-automation/blob/master/for-scripters/Python/advanced-view-api.ipynb
-                    print('Alrighty GRAY OUT SUCKAH +++++++++++++++++++============++++++++++++++++')
+                    print('Alrighty GRAY OUT +++++++++++++++++++============++++++++++++++++')
+
+                    # network_suid = node_edge_network.get_id()
+                    # print('This object references to Cytoscape network with SUID ' + str(network_suid) + '\n')
+                    # print('And its name is: ' + str(node_edge_network.get_network_value(column='name')) + '\n')
+                    #
+                    # nodes = node_edge_network.get_node_table()
+                    # edges = node_edge_network.get_edge_table()
+                    # nodes.columns = nodes.columns.str.replace("[ ]", "_")
+                    #
+                    # # filter out the user desired data
+                    # filtered_df = nodes.query("reason_to_exist == 'Presentation'").reset_index(
+                    #     drop=True)
+                    #
+                    # print('filtered df')
+                    # print(filtered_df)
+                    #
+                    # if 'invert' in self.core_details.columns:
+                    #     if self.core_details.at[0, 'invert'] != 0:
+                    #         # Invert
+                    #         new_df = nodes.merge(filtered_df, on=['id_original', 'name'])
+                    #         filtered_df = nodes[(~nodes.id.isin(new_df.id))]
+                    #         print('this is the new filtered df')
+                    #         print(filtered_df)
+                    #         print('Query needs to be modified')
+                    #
+                    # # Create a new style
+                    # gray_style = cy.style.create('gray_out_style')
+                    # # Get a reference to the existing style
+                    # default_style = cy.style.create('Epi_Explorer_style')
+                    #
+                    # print('______ get default stuff _______')
+                    # print(default_style.get_name())
+                    # print('views????')
+                    #
+                    # for index, row in nodes.iterrows():
+                    #
+                    #     print(nodes.at[index, 'SUID'], ' : ', node_edge_network.get_node_value(id=nodes[index]))
+                    #
+                    #
+                    # key_value_pair = {}
+                    # for index, row in filtered_df.iterrows():
+                    #     filtered_id = filtered_df.at[index, 'SUID']
+                    #     key_value_pair[str(filtered_id)] = '#ff0000'
+                    #
+                    # gray_style.create_discrete_mapping(column='SUID', col_type='string',
+                    #                                    vp='NODE_FILL_COLOR', mappings=key_value_pair)
+                    #
+                    # cy.style.apply(gray_style, node_edge_network)
+
                     # Check if the gray out is set to True
-                    if self.core_details.at[0, 'gray']:
-                        user_query = self.core_details.at[0, 'query']
+                    if core_details.at[0, 'gray']:
+                        user_query = core_details.at[0, 'query']
                         print(user_query)
 
-                        if 'invert' in self.core_details.columns:
-                            if self.core_details.at[0, 'invert'] != 0:
-                                # TODO write code to modify query, basically the stuff I did below
-                                print('Query needs to be modified')
-
                         # Get the network from cytoscape
-                        view_id_list = node_edge_network.get_views()
-                        # TODO check why format == 'view' doesn't work!!!!!!
-                        view1 = node_edge_network.get_view(view_id_list[0], fmt='view')
-                        my_style_2 = node_edge_network.get_view(view_id_list[0])
+                        view_id_list = self.node_edge_network.get_views()
+                        view1 = self.node_edge_network.get_view(view_id_list[0], fmt='view')
+                        my_style_2 = self.node_edge_network.get_view(view_id_list[0])
 
                         # Get nodes and edges as a df
                         view_df = pd.DataFrame.from_dict(my_style_2, orient='index')
@@ -253,11 +303,14 @@ class CytoscapeIntegration:
                         filtered_df = correct_node_data_df.query("reason_to_exist == 'Presentation'").reset_index(
                             drop=True)
 
-                        # Remove the filtered data and make a new df
-                        # new_df = correct_node_data_df.merge(filtered_df, on=['id_original', 'name'])
-                        # correct_node_data_df[(~correct_node_data_df.id.isin(new_df.id))]
-                        # print('this is the new df')
-                        # print(new_df)
+                        if 'invert' in core_details.columns:
+                            if core_details.at[0, 'invert'] != 0:
+                                # Invert
+                                new_df = correct_node_data_df.merge(filtered_df, on=['id_original', 'name'])
+                                filtered_df = correct_node_data_df[(~correct_node_data_df.id.isin(new_df.id))]
+                                print('this is the new filtered df')
+                                print(filtered_df)
+                                print('Query needs to be modified')
 
                         # Get node/edge views as a Python dictionary
                         node_views_dict = view1.get_node_views_as_dict()
@@ -300,12 +353,12 @@ class CytoscapeIntegration:
                         print(key_value_pair)
 
                         view1.update_node_views(visual_property='NODE_FILL_COLOR', values=key_value_pair)
-                        Image(node_edge_network.get_png(height=400))
+                        Image(self.node_edge_network.get_png(height=400))
                         self.filter_data()
 
-        cy.style.apply(my_style, node_edge_network)
+        # cy.style.apply(my_style, node_edge_network)
 
-        cy.layout.fit(network=node_edge_network)
-        Image(node_edge_network.get_png(height=400))
+        self.cy.layout.fit(network=self.node_edge_network)
+        Image(self.node_edge_network.get_png(height=400))
 
         return cytoscape_successful
